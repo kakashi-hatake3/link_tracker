0c3d74e4fce0ad940864dde7b0b2271347357759
kakashi-hatake3 <mayorov.art7@gmail.com>
2025-02-19 18:28:51 +0300

    feat: добавил схемы ответов для апи

---
+++ src/models.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, Field, HttpUrl
+from typing import List, Optional
+
+class Link(BaseModel):
+    url: HttpUrl
+    description: Optional[str] = None
+
+class LinkUpdate(BaseModel):
+    id: int
+    url: HttpUrl
+    description: Optional[str] = None
+    tg_chat_ids: List[int] = Field(alias="tgChatIds")
+
+class ApiErrorResponse(BaseModel):
+    description: str
+    code: str
+    exception_name: Optional[str] = Field(None, alias="exceptionName")
+    exception_message: Optional[str] = Field(None, alias="exceptionMessage")
+    stacktrace: Optional[List[str]] = None
+
+class User(BaseModel):
+    chat_id: int
+    tracked_links: List[Link] = Field(default_factory=list) 
