4d5ad4a800d5c6fe09e8080e2cc21996e906891f
kakashi-hatake3 <mayorov.art7@gmail.com>
2025-02-19 18:22:15 +0300

    feat: изменил импорт апи

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter
-
-from . import ping
+from src.api.updates import router as updates_router
 
 __all__ = ("router",)
 
 router = APIRouter()
-router.include_router(ping.router, tags=["ping"])
+router.include_router(updates_router)
