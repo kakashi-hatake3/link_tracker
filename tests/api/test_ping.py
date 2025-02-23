6c23f8cf71819d787c5d4791ce334ba4812844be
kakashi-hatake3 <mayorov.art7@gmail.com>
2025-02-24 00:17:21 +0300

    test: изменил тест для ping pong

--- tests/api/test_ping.py
+++ tests/api/test_ping.py
@@ -1,12 +1,9 @@
-from fastapi import status
-from starlette.testclient import TestClient
+from fastapi.testclient import TestClient
+from src.server import app
 
+client = TestClient(app)
 
-def test_api_ping(
-    test_client: TestClient,
-) -> None:
-    response = test_client.get("/api/v1/ping")
-    assert response.status_code == status.HTTP_200_OK
-    response_json = response.json()
-    assert "pong" in response_json
-    assert response_json["pong"] == "ok"
+def test_api_ping():
+    response = client.get("/api/v1/ping")
+    assert response.status_code == 200
+    assert response.json() == {'pong': 'ok'}
