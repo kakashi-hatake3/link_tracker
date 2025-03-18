baf63c5595127f4307f0eb2c9f529fe686cee502
kakashi-hatake3 <mayorov.art7@gmail.com>
2025-03-18 21:19:43 +0300

    feat: добавил проверку на запуск на CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,12 +19,12 @@ def mock_event() -> Mock:
     return event
 
 
-def convert_to_jdbc(db_url: str) -> str:
+def convert_to_jdbc(db_url: str, use_host_internal: bool = True) -> str:
     """Преобразует URL подключения в формат JDBC, который требуется Liquibase."""
     parsed = urllib.parse.urlparse(db_url)
     host = parsed.hostname
     port = parsed.port
-    if host == "localhost":
+    if use_host_internal and host == "localhost":
         host = "host.docker.internal"
     jdbc_url = f"jdbc:postgresql://{host}:{port}{parsed.path}"
     return jdbc_url
@@ -35,21 +35,38 @@ def postgres_container() -> str:
     with PostgresContainer("postgres:14") as postgres:
         db_url = postgres.get_connection_url(driver="psycopg").replace("postgresql://", "postgresql+psycopg://", 1)
 
-        jdbc_url = convert_to_jdbc(db_url)
-
-        subprocess.run([
-            "docker", "run", "--rm",
-            "--add-host=host.docker.internal:host-gateway",
-            "-v", f"{os.getcwd()}/migrations:/changesets",
-            "liquibase/liquibase:4.29",
-            "--searchPath=/changesets",
-            "--changelog-file=master.xml",
-            "--driver=org.postgresql.Driver",
-            f"--url={jdbc_url}",
-            "--username=postgres",
-            "--password=postgres",
-            "update"
-        ], check=True)
+        ci_env = os.environ.get("CI")
+        if ci_env:
+            jdbc_url = convert_to_jdbc(db_url, use_host_internal=False)
+            docker_command = [
+                "docker", "run", "--rm",
+                "--network=host",
+                "-v", f"{os.getcwd()}/migrations:/changesets",
+                "liquibase/liquibase:4.29",
+                "--searchPath=/changesets",
+                "--changelog-file=master.xml",
+                "--driver=org.postgresql.Driver",
+                f"--url={jdbc_url}",
+                "--username=postgres",
+                "--password=postgres",
+                "update"
+            ]
+        else:
+            jdbc_url = convert_to_jdbc(db_url, use_host_internal=True)
+            docker_command = [
+                "docker", "run", "--rm",
+                "--add-host=host.docker.internal:host-gateway",
+                "-v", f"{os.getcwd()}/migrations:/changesets",
+                "liquibase/liquibase:4.29",
+                "--searchPath=/changesets",
+                "--changelog-file=master.xml",
+                "--driver=org.postgresql.Driver",
+                f"--url={jdbc_url}",
+                "--username=postgres",
+                "--password=postgres",
+                "update"
+            ]
+        subprocess.run(docker_command, check=True)
 
         yield db_url
 
